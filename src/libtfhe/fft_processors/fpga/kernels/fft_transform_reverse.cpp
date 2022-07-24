0b1fcbc6ec738ed0ed6490d3b42bf0ed07dea12d
Emerson Jacobson <emwjacobson@gmail.com>
2022-07-23 22:18:44 -0700

    Fixed gitignore error to include constants, fixed trig error

--- src/libtfhe/fft_processors/fpga/kernels/fft_transform_reverse.cpp
+++ src/libtfhe/fft_processors/fpga/kernels/fft_transform_reverse.cpp
@@ -22,7 +22,8 @@ extern "C" {
 		memcpy(&imag, _imag, sizeof(double) * N);
 
 		// Bit-reversed addressing permutation
-		for (uint64_t i = 0; i < N; i++) {
+		uint64_t i;
+		for (i = 0; i < N; i++) {
 			uint64_t j = bit_reversed[i];
 			if (i < j) {
 				double tp0re = real[i];
@@ -38,11 +39,11 @@ extern "C" {
 
 		// Size 2 merge (special)
 		if (N >= 2) {
-			for (uint64_t i = 0; i < N; i += 2) {
+			for (i = 0; i < N; i += 2) {
 				double tpre = real[i];
 				double tpim = imag[i];
-				real[i] = real[i] + real[i + 1];
-				imag[i] = imag[i] + imag[i + 1];
+				real[i] += real[i + 1];
+				imag[i] += imag[i + 1];
 				real[i + 1] = tpre - real[i + 1];
 				imag[i + 1] = tpim - imag[i + 1];
 			}
@@ -50,47 +51,49 @@ extern "C" {
 
 		// Size 4 merge (special)
 		if (N >= 4) {
-			for (uint64_t i = 0; i < N; i += 4) {
+			for (i = 0; i < N; i += 4) {
 				// Even indices
 				double tpre, tpim;
 				tpre = real[i];
 				tpim = imag[i];
-				real[i] = real[i] + real[i + 2];
-				imag[i] = imag[i] + imag[i + 2];
+				real[i] += real[i + 2];
+				imag[i] += imag[i + 2];
 				real[i + 2] = tpre - real[i + 2];
 				imag[i + 2] = tpim - imag[i + 2];
 				// Odd indices
 				tpre = real[i + 1];
 				tpim = imag[i + 1];
-				real[i + 1] = real[i + 1] - imag[i + 3];
-				imag[i + 1] = imag[i + 1] + real[i + 3];
-				tpre = tpre + imag[i + 3];
-				tpim = tpim - real[i + 3];
+				real[i + 1] -= imag[i + 3];
+				imag[i + 1] += real[i + 3];
+				tpre += imag[i + 3];
+				tpim -= real[i + 3];
 				real[i + 3] = tpre;
 				imag[i + 3] = tpim;
 			}
 		}
 
 		// Size 8 and larger merges (general)
-		uint64_t off;
-		uint64_t halfsize;
 		double *trigtables = trig_tables;
-		for (uint64_t size = 8; size <= N; size <<= 1) {
-			for (uint64_t i = 0; i < N; i += size) {
-				off = 0;
-				halfsize = size >> 1;
-				for (uint64_t j = 0; j < halfsize; j++) {
-					uint64_t vi = i + j;  // Vector index
-					uint64_t ti = off;    // Table index
-					double re = real[vi + halfsize];
-					double im = imag[vi + halfsize];
-					double tpre = re * trigtables[ti] + im * trigtables[ti + 4];
-					double tpim = im * trigtables[ti] - re * trigtables[ti + 4];
-					real[vi + halfsize] = real[vi] - tpre;
-					imag[vi + halfsize] = imag[vi] - tpim;
-					real[vi] = real[vi] + tpre;
-					imag[vi] = imag[vi] + tpim;
-					off += 8;
+		uint64_t size;
+		for (size = 8; size <= N; size <<= 1) {
+			uint64_t halfsize = size >> 1;
+			uint64_t i;
+			for (i = 0; i < N; i += size) {
+				uint64_t j, off;
+				for (j = 0, off = 0; j < halfsize; j += 4, off += 8) {
+					uint64_t k;
+					for (k = 0; k < 4; k++) {  // To simulate x86 AVX 4-vectors
+						uint64_t vi = i + j + k;  // Vector index
+						uint64_t ti = off + k;    // Table index
+						double re = real[vi + halfsize];
+						double im = imag[vi + halfsize];
+						double tpre = re * trigtables[ti] + im * trigtables[ti + 4];
+						double tpim = im * trigtables[ti] - re * trigtables[ti + 4];
+						real[vi + halfsize] = real[vi] - tpre;
+						imag[vi + halfsize] = imag[vi] - tpim;
+						real[vi] += tpre;
+						imag[vi] += tpim;
+					}
 				}
 			}
 			if (size == N)
